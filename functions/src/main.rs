bf6e59948857f0480ca9cc241d7e56956e0d6952
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2023-05-21 19:24:52 -0700

    finished chapter 3

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     println!("main");
 
-    a_function();
+    let y = a_function(10);
+    println!("returned {y}");
 }
 
-fn a_function(x: i32) {
-    println!("another {x}");
+fn a_function(x: i32) -> i32 {
+    println!("a_function {x}");
+    x * 2
 }
