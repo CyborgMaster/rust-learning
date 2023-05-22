84569c85875c5b68a939f4132a2f280766bade52
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2023-05-21 18:58:50 -0700

    couple more chapters

---
+++ functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("main");
+
+    a_function();
+}
+
+fn a_function() {
+    println!("another");
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let t: [f64; 10] = [5.0; 10];
+    println!("first: {:?}", t);
+}
