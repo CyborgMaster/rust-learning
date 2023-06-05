b69322ea47ab7720e5cffe4b4045946fc32134c5
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2023-06-04 17:18:04 -0700

    correctly deal with consonant clusters

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -3,6 +3,7 @@ use onig::Regex;
 
 fn pig_latin(s: &str) -> String {
     lazy_static! {
+        // Split at word boundaries, but don't count single quotes as a boundary.
         static ref WORDS: Regex = Regex::new(r"(?<!')\b(?!')").unwrap();
     }
     WORDS.split(s).map(pig_word).collect()
@@ -18,26 +19,29 @@ fn pig_word(word: &str) -> String {
         return word.to_string();
     }
 
-    // dbg!(word);
+    let first_vowel_pos = word.find(|c| is_vowel(c)).unwrap();
+    if first_vowel_pos == 0 {
+        return word.to_string() + "-hay";
+    }
+    let (mut first, mut rest) = word.split_at(first_vowel_pos);
 
-    let mut first = first_char.to_string();
-    let mut rest = &word[first_char.len_utf8()..];
-
-    let upcase_rest: String;
-    match first_char.to_ascii_lowercase() {
-        'a' | 'e' | 'i' | 'o' | 'u' => {
-            first = "h".to_string();
-            rest = word;
-        }
-        _ if first_char.is_uppercase() => {
-            first = first_char.to_lowercase().collect();
-            upcase_rest = upcase_first_char(rest);
-            rest = &upcase_rest;
-        }
-        _ => (),
+    let first_lowercase: String;
+    let rest_uppercase: String;
+    if first_char.is_uppercase() {
+        first_lowercase = first.to_lowercase();
+        first = &first_lowercase;
+        rest_uppercase = upcase_first_char(rest);
+        rest = &rest_uppercase;
     }
 
     format!("{}-{}ay", rest, first)
+}
+
+fn is_vowel(c: char) -> bool {
+    match c.to_ascii_lowercase() {
+        'a' | 'e' | 'i' | 'o' | 'u' => true,
+        _ => false,
+    }
 }
 
 fn upcase_first_char(s: &str) -> String {
